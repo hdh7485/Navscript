a7d9bbb5db70996b99965c04232c4046fafc49e5
Donghee Han <hdh7485@gmail.com>
2018-06-29 13:46:29 +0900

    Add numbering something and profile_messages.

--- Final_0627_hdh.py
+++ Final_0627_hdh.py
@@ -42,28 +42,36 @@ messages = ["What's the {OTHER:COMMON} for this afternoon?",
         "Are there any {OTHER:COMMON} on my {OTHER:COMMON}?",
         "Will it rain tomorrow in {LOCATION:PROPER}?"
         ]
+'''
+Move this strings to profile_messages.txt
 
-messages2 = ["What's the SOMETHING for this afternoon?",
-        "What's the SOMETHING like on my SOMETHING?",
-        "Show me a SOMETHING on WASHINGTON and WASHINGTON.",
+messages2 = ["What's the SOMETHING1 for this afternoon?",
+        "What's the SOMETHING1 like on my SOMETHING2?",
+        "Show me a SOMETHING1 on WASHINGTON and WASHINGTON.",
         "Can you find me a WASHINGTON with WASHINGTON nearby?",
         "Find a WASHINGTON along WASHINGTON.",
-        "Find the cheapest indoor SOMETHING within 500 meters of my SOMETHING.",
-        "Okay, can you find me a WASHINGTON on my SOMETHING that has a WASHINGTON?",
-        "Find SOMETHING near WASHINGTON that accepts SOMETHING and has a SOMETHING.",
+        "Find the cheapest indoor SOMETHING1 within 500 meters of my SOMETHING2.",
+        "Okay, can you find me a WASHINGTON on my SOMETHING1 that has a WASHINGTON?",
+        "Find SOMETHING1 near WASHINGTON that accepts SOMETHING2 and has a SOMETHING3.",
 
         "Navigate to WASHINGTON.",
-        "What's my SOMETHING to WASHINGTON?",
-        "Show me alternative SOMETHING.",
-        "Reroute using SOMETHING.",
+        "What's my SOMETHING1 to WASHINGTON?",
+        "Show me alternative SOMETHING1.",
+        "Reroute using SOMETHING1.",
 
         "Drive to WASHINGTON.",
-        "What's my SOMETHING?",
+        "What's my SOMETHING1?",
         "Can I make tomorrow's 10am {EVENT:COMMON} without recharging?",
-        "What's SOMETHING like on the WASHINGTON?",
-        "Are there any SOMETHING on my SOMETHING?",
+        "What's SOMETHING1 like on the WASHINGTON2?",
+        "Are there any SOMETHING1 on my SOMETHING2?",
         "Will it rain tomorrow in WASHINGTON?"
         ]
+with open("profile_messages.txt") as f:
+    messages2 = f.readlines()
+    '''
+
+messages2 = [line.rstrip('\n') for line in open('profile_messages.txt')]
+print(messages2)
 
 scripts = ["[SEARCH FROM:SOMETHING WHERE:HERE WHEN:AFTERNOON]",
          "[SEARCH FROM:SOMETHING WHERE:SOMETHING]",
@@ -86,9 +94,16 @@ scripts = ["[SEARCH FROM:SOMETHING WHERE:HERE WHEN:AFTERNOON]",
          "[MODE WEATHERFORECAST WHERE:[SEARCH KEYWORD:WASHINTON] WHEN:TOMORROW]"
           ]
 
+print('Input: {}'.format(lines))
+
+location_pool = ['place1', 'place2', 'place3', 'place4', 'place5']
+#location_pool = ['WASHINGTON', 'SEOUL', 'MADRID', 'LONDON', 'BEIJING']
+other_pool = ['SOMETHING1', 'SOMETHING2', 'SOMETHING3', 'SOMETHING4', 'SOMETHING5']
 
 google_entity_type ={0:'UNKNOWN', 1:'PERSON', 2:'LOCATION', 3:'ORGANIZATION', 4:'EVENT', 5:'WORK_OF_ART', 6:'CONSUMER_GOOD', 7:'OTHER'}
-entity_type ={0:'X', 1:'DAVID', 2:'WASHINTON', 3:'SCHOOL', 4:'MEETTING', 5:'MONALISA', 6:'NOTEBOOK', 7:'SOMETHING'}
+entity_type ={0:'X', 1:'DAVID', 2:'WASHINTON', 3:'SCHOOL', 4:'MEETTING', 5:'MONALISA', 6:'NOTEBOOK', 7:iter(other_pool)}
+entity_type2 ={0:'X', 1:'DAVID', 2:'WASHINTON', 3:'SCHOOL', 4:'MEETTING', 5:'MONALISA', 6:'NOTEBOOK', 7:iter(other_pool)}
+#entity_type ={0:'X', 1:'DAVID', 2:'WASHINTON', 3:'SCHOOL', 4:'MEETTING', 5:'MONALISA', 6:'NOTEBOOK', 7:'SOMETHING'}
 i_entity_type ={'X':0, 'DAVID':1, 'WASHINTON':2, 'SCHOOL':3, 'MEETTING':4, 'MONALISA':5, 'NOTEBOOK':6, 'SOMETHING':7}
 
 client = language.LanguageServiceClient()
@@ -98,11 +113,12 @@ document = types.Document(
         type=enums.Document.Type.PLAIN_TEXT)
 
 entities = client.analyze_entities(document).entities
+print('Entities: {}'.format(entities))
 
 result = lines
 
 for entity in entities:
-    result = result.replace(entity.name, entity_type[entity.type] )
+    result = result.replace(entity.name, next(entity_type[entity.type]))
 
 print("Replace nouns: {}".format(result))
 
@@ -143,9 +159,7 @@ for i, message_embedding in enumerate(message_embeddings):
       minimum = error
       minimum_index = i
 
-print("\n\n")
-
-
+print("\n")
 print("Minimum RMSE value: {}".format(minimum))
 print("Most similar script: {}".format(scripts[minimum_index]))
 print("Estimation: {}".format(minimum_index))
@@ -160,7 +174,8 @@ google=[]
 for entity in entities:
     # Dict_entitis[entity.name]=entity_type[entity.type]
     google.append(google_entity_type[entity.type])
-    K.append(entity_type[entity.type])
+    #K.append(entity_type[entity.type])
+    K.append(next(entity_type2[entity.type]))
     V.append(entity.name)
 
 
