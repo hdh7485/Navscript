65a47a5c16de9a68a041b680995a768257a4fa5f
Donghee Han <hdh7485@gmail.com>
2018-07-14 11:58:43 +0900

    Add navscript tags convesrion.

--- data_generator.py
+++ data_generator.py
@@ -1,4 +1,4 @@
-def make_data():
+def replace_tags(string_list):
     place = ['burger king', 'burgerking', 'restroom', 'restroom facility',  'super market', 'supermarket', 'school', 'starbucks', 'coffee shop', 'cafe', 'college', 'gas station', 'park', 'department store', 'restaurent', 'indoor parking', 'outdoor parking', 'my destination']
     other = ['route', 'traffic camera', 'traffic', 'speed camera']
     weather = ['weather', 'weather forecast']
@@ -9,54 +9,10 @@ def make_data():
     service = ['valet service', 'credit card']
     search_place = ['San Francisco Museum of Modern Art', 'Downtown Berkeley', 'Bay Bridge', 'Oakland', 'Seoul', 'New York', 'Wahinton', 'Chicago']
 
-    sentences = [
-            "What's the [weather] for this [time]?",
-            "What's the [other] like on my [other]?",
-            "Show me a [other] on [geocode] and [geocode].",
-            "Can you find me a [place] with [place] nearby?",
-            "Find a [place] along route.",
-            "Find the cheapest [place] within [distance] of [place].",
-            "Okay, can you find me a [place] on my route that has a [place]?",
-            "Find [place] near destination that accepts [service] and has a [service].",#Isn't it need range(near) query?
-
-            "Navigate to [search_place].",
-            "What's my ETA to destination? ",
-            "Show me alternative routes.",
-            "Reroute using [geocode].",
-
-            "Drive to [search_place].",
-            "What's my drive range?",
-            "Can I make [time] [event] without recharging?",
-            "What's traffic like on the [search_place]?",
-            "Are there any [other] on my route?",
-            "Will it rain [time] in [search_place]?"
-            ]
-
-    navscripts = [
-            "[SEARCH FROM:[weather] WHERE:HERE WHEN:[time]]",
-            "[SEARCH FROM:[other] WHERE:[other]]",
-            "[SEARCH FROM:[ohter] WHERE:[SEARCH GEOCODE WHERE:[geocode] and [geocode]]]",
-            "[SEARCH FROM:[place] WHERE:NEARBY WITH:[place]]",
-            "[SEARCH ONE FROM:[place] WHERE:ALONGROUTE]",
-            "[SEARCH ONE FROM:[place] WHERE:[place] RANGE:[distance] WITH:[SORT PRICE ASC]]",
-            "[SEARCH ONE FROM:[place] WHERE:ONROUTE WITH:[place]]",
-            "[SEARCH ONE FROM:[place] WITH:[service] WITH:[service]]",
-
-            "[ROUTE TO:[SEARCH KEYWORD:[search_place]]]",
-            "[ROUTE INFO:ETA]",
-            "[ROUTE ALTROUTE]",
-            "[ROUTE ALTROUTE USE:[SEARCH LINKS:[geocode]]]",
-
-            "[MODE GUIDANCE WITH:[ROUTE TO:[SEARCH KEYWORD:[search_place]]]]",
-            "[MODE DRIVERANGE]",
-            "[MODE DRIVERANGE TO:[SEARCH KEYWORD:[time] [event] FROM:SCHEDULE WHEN:[time]] WITH:[VOICERESPONSE TEMPLATE:YES/NO*]]",
-            "[MODE TRAFFIC [SEARCH FROM:TRAFFIC WHERE:[SEARCH KEYWORD:[search_place]] WITH:[VOICERESPONSE TEMPLATE:""*]",
-            "[MODE [other] WHERE:ONROUTE WITH:[VOICERESPONSE TEMPLATE:""*]]",
-            "[MODE WEATHERFORECAST WHERE:[SEARCH KEYWORD:[place]] WHEN:[time]]"
-            ]
+    class_lists = []
 
     generated_sentences = []
-    for sentence in sentences:
+    for sentence in string_list:
         if not sentence.find('[place]') == -1:
             for p in place:
                 generated_sentences.append(sentence.replace("[place]", p))
@@ -119,12 +75,64 @@ def make_data():
         else:
             generated_sentences8.append(sentence)
 
-    result = generated_sentences8
-    print(result)
+    replaced_result = generated_sentences8
+    return replaced_result
+
+def make_data():
+    sentences = [
+            "What's the [weather] for this [time]?",
+            "What's the [other] like on my [other]?",
+            "Show me a [other] on [geocode] and [geocode].",
+            "Can you find me a [place] with [place] nearby?",
+            "Find a [place] along route.",
+            "Find the cheapest [place] within [distance] of [place].",
+            "Okay, can you find me a [place] on my route that has a [place]?",
+            "Find [place] near destination that accepts [service] and has a [service].",#Isn't it need range(near) query?
+
+            "Navigate to [search_place].",
+            "What's my ETA to destination? ",
+            "Show me alternative routes.",
+            "Reroute using [geocode].",
+
+            "Drive to [search_place].",
+            "What's my drive range?",
+            "Can I make [time] [event] without recharging?",
+            "What's traffic like on the [search_place]?",
+            "Are there any [other] on my route?",
+            "Will it rain [time] in [search_place]?"
+            ]
+
+    navscripts = [
+            "[SEARCH FROM:[weather] WHERE:HERE WHEN:[time]]",
+            "[SEARCH FROM:[other] WHERE:[other]]",
+            "[SEARCH FROM:[ohter] WHERE:[SEARCH GEOCODE WHERE:[geocode] and [geocode]]]",
+            "[SEARCH FROM:[place] WHERE:NEARBY WITH:[place]]",
+            "[SEARCH ONE FROM:[place] WHERE:ALONGROUTE]",
+            "[SEARCH ONE FROM:[place] WHERE:[place] RANGE:[distance] WITH:[SORT PRICE ASC]]",
+            "[SEARCH ONE FROM:[place] WHERE:ONROUTE WITH:[place]]",
+            "[SEARCH ONE FROM:[place] WITH:[service] WITH:[service]]",
+
+            "[ROUTE TO:[SEARCH KEYWORD:[search_place]]]",
+            "[ROUTE INFO:ETA]",
+            "[ROUTE ALTROUTE]",
+            "[ROUTE ALTROUTE USE:[SEARCH LINKS:[geocode]]]",
+
+            "[MODE GUIDANCE WITH:[ROUTE TO:[SEARCH KEYWORD:[search_place]]]]",
+            "[MODE DRIVERANGE]",
+            "[MODE DRIVERANGE TO:[SEARCH KEYWORD:[time] [event] FROM:SCHEDULE WHEN:[time]] WITH:[VOICERESPONSE TEMPLATE:YES/NO*]]",
+            "[MODE TRAFFIC [SEARCH FROM:TRAFFIC WHERE:[SEARCH KEYWORD:[search_place]] WITH:[VOICERESPONSE TEMPLATE:""*]",
+            "[MODE [other] WHERE:ONROUTE WITH:[VOICERESPONSE TEMPLATE:""*]]",
+            "[MODE WEATHERFORECAST WHERE:[SEARCH KEYWORD:[place]] WHEN:[time]]"
+            ]
+    sentence_result = replace_tags(sentences)
+    script_result = replace_tags(navscripts)
+    print(sentence_result)
+    print(script_result)
 
     with open('generate_test.txt', 'w') as f:
-        for i in result:
-            f.write("{}\n".format(i))
+        for sentence, navscript in zip(sentence_result, script_result):
+            f.write("{}\n{}\n".format(sentence, navscript))
+
     return
 
 if __name__ == "__main__":
