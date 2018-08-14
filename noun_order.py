f21c71b6dc661e3bb60423edc46fb974cca002de
Donghee Han <hdh7485@gmail.com>
2018-08-14 11:09:04 +0900

    Add test case in noun_order.py

--- noun_order.py
+++ noun_order.py
@@ -6,12 +6,13 @@ def check_order(sentence, nouns):
     return result
 
 def main():
-    sentence = 'Where is a burgerking along the route.'
-    #nouns = ['burgerking', 'route']
-    nouns = ['route', 'burgerking', 'along', 'Where']
-    print('input:{}'.format(sentence))
-    print('input:{}'.format(nouns))
-    print('output:{}'.format(check_order(sentence, nouns)))
+    sentences_set = ['Where is a burgerking along the route.', 'What\'s the traffic like on my route?', 'Can you find me a gas station with restroom facilities nearby?']
+    nouns_set = [['route', 'burgerking', 'along', 'Where'], ['route', 'traffic'], ['facilities', 'restroom', 'gas station']]
+
+    for sentence, nouns in zip(sentences_set, nouns_set):
+        print('input:{}'.format(sentence))
+        print('input:{}'.format(nouns))
+        print('output:{}'.format(check_order(sentence, nouns)))
 
 if __name__ == '__main__':
     main()
